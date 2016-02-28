784740eff815c70320d3687761afb5643830dba0
HuyGemini <kimera004@gmail.com>
2016-02-29 02:25:49 +0700

    Update
    Thêm xoá + tìm kiếm + in + 1 số lệnh linh tinh

--- ContactList.h
+++ ContactList.h
@@ -18,12 +18,18 @@ void help();
 Node *Insert_toHead(Node *First, information X);
 Node *Insert_Middle(Node *Pred, information X);
 Node *Insert_toLast(Node *First, information X );
-void Delete(Node *Pred);
+
 Node *Delete_Head(Node *First);
-void Delete_Last();
+void Delete_Middle(Node *Pred);
+void Delete_Last(Node *First);
+void Delete(Node *First, int location);
+
 int isEmpty(Node *First);
 Node *MakeNull(Node *First);
 void Print(Node *First);
 
 Node *CreateContact(Node *First);
+
+int Search(Node *First,int k);
+void Print_forSearch(Node *First, int location);
 
